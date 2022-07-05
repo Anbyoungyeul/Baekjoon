8d4c298ca12407d450ccb93d129b92b9576f5772
byeongyeol <101905747+byeongyeolahn@users.noreply.github.com>
2022-07-05 23:50:37 +0900

    Update 10818.c

--- 10818.c
+++ 10818.c
@@ -2,14 +2,14 @@
 
 int main(void)
 {
-    int n=0, count=0, min=100000, max=-100000; # 최댓값 최솟값 설정
-    scanf("%d", &count);
-    for(int i=0;i<count;i++)
+    int n=0, count=0, min=100000, max=-100000; // 최댓값 최솟값 설정
+    scanf("%d", &count); // %d, %s 구분
+    for(int i=0;i<count;i++) //입력받은 횟수 만큼
     {
         scanf("%d", &n);
-        if(n>max)
+        if(n>max) // 최댓값 업데이트
             max=n;
-        if(n<min)
+        if(n<min) //최솟값 업데이트
             min=n;
     }
     printf("%d %d", min,max); 
