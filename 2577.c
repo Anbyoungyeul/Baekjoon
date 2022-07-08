e928f258625b8d8c3351ddf7b8cf41355a73a238
byeongyeol <101905747+byeongyeolahn@users.noreply.github.com>
2022-07-09 01:46:33 +0900

    2577_c tring

---
+++ 2577.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void){
+    int num1=0, num2=0, num3=0;
+    int mul=0;
+    string mul_str =''
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    scanf("%d", &num3);
+    mul = num1*num2*num3;
+    mul_str=itoa(mul, len(mul), 10);
+    
+}
